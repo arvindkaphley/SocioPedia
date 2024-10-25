0debbe6d56e1c0ac2712ed9a83ebfd9193cb6833
arvindkaphley <kaphleyarvind19@gmail.com>
2024-10-25 09:24:28 +0530

    Api pushed

---
+++ api/index.js
@@ -0,0 +1,34 @@
+import express from "express"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+import commentRoutes from "./routes/comments.js"
+import likeRoutes from "./routes/likes.js"
+import authRoutes from "./routes/auth.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app=express()
+
+
+// middlewares
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  });
+app.use(express.json())
+app.use(
+    cors({
+        origin:"http://localhost:5173",
+    })
+)
+app.use(cookieParser())
+
+app.use("/api/auth",authRoutes)
+app.use("/api/users",userRoutes)
+app.use("/api/posts",postRoutes)
+app.use("/api/comments",commentRoutes)
+app.use("/api/likes",likeRoutes)
+
+app.listen(8800,()=>{
+    console.log("API Working!")
+})
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,36 +1,69 @@
-import React, { useContext } from 'react'
-import "./login.scss"
-import { Link } from 'react-router-dom'
-import { AuthContext } from '../../context/authContext'
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import "./login.scss";
 
 const Login = () => {
-  const {login}=useContext(AuthContext)
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setErr] = useState(null);
 
-  const handleLogin=()=>{
-    login();
-  }
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs);
+      navigate("/")
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
   return (
-    <div className='login'>
-      <div className='card'>
-        <div className='left'>
-            <h1>Sociopedia.</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae modi velit distinctio voluptates, adipisci nihil provident nostrum ipsum eum ea harum officiis minima atque quaerat soluta. Aliquid consequatur nesciunt pariatur.</p>
-            <span>Don't have an account?</span>
-            <Link to="/register">
-                <button>Register</button>
-            </Link>
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
         </div>
-        <div className='right'>
-            <h1>Login</h1>
-            <form>
-                <input type="text" placeholder='Username'/>
-                <input type="text" placeholder='Password'/>
-                <button onClick={handleLogin}>Login</button>
-            </form>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            {err && err}
+            <button onClick={handleLogin}>Login</button>
+          </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
